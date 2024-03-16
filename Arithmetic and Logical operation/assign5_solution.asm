b8e0235f82e1b2c0d9ba2525ad708d7b1cfbd222
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-03-16 23:44:27 +0700

    Update assign5_solution.asm

--- Arithmetic and Logical operation/assign5_solution.asm
+++ Arithmetic and Logical operation/assign5_solution.asm
@@ -1,1 +1,17 @@
+.text
+
+main:
+li $s0, 5 # gia tri khoi tao cho $s0
+
+# Nhân $s0 voi 2 (tuong ung dich trai 1 bit)
+sll $s1, $s0, 1 # $s1 = $s0 * 2
+
+# Nhân $s0 voi 4 (tuong ung dich trai 2 bit)
+sll $s2, $s0, 2 # $s2 = $s0 * 4
+
+# Nhân $s0 voi 8 (tuong ung dich trai 3 bit)
+sll $s3, $s0, 3 # $s3 = $s0 * 8
+
+# Nhân $s0 voi 16 (tuong ung dich trai 4 bit)
+sll $s4, $s0, 4 # $s4 = $s0 * 16
 
