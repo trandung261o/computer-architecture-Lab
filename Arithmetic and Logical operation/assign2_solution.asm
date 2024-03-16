eb25e2e88a733e0c7986e26bbb80361bd9645fd8
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-03-16 23:37:27 +0700

    Update assign2_solution.asm

--- Arithmetic and Logical operation/assign2_solution.asm
+++ Arithmetic and Logical operation/assign2_solution.asm
@@ -1,1 +1,16 @@
+.text
+li $s0, 0x0563 # load test value 
+
+# Extract MSB of $s0
+srl $t0, $s0, 24
+
+# Clear LSB of $s0
+andi $s0, $s0, 0xFFFFFF00
+
+# Set LSB of $s0 (bits 7 to 0 are set to 1)
+ori $s0, $s0, 0xFF
+
+# Clear $s0 (s0=0, must use logical instructions)
+xor $s0, $s0, $s0
+
 
