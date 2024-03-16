4469c0015bd642f38fdfc32b4303e6dade128878
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-03-16 23:42:51 +0700

    Update assign4_solution.asm

--- Arithmetic and Logical operation/assign4_solution.asm
+++ Arithmetic and Logical operation/assign4_solution.asm
@@ -1,1 +1,13 @@
+.text
+start:
+	li $s0, 0x7FFFFFFF 	
+	li $s1, 0x00000001 	
 
+	li 	$t0,0 		#ko tran so la trang thai mac dinh
+	addu 	$s2,$s0,$s1 	# s2 = s0 + s1
+	xor 	$t1,$s0,$s1 	#kiem tra xem $s0 và $s1 co cung dau khong
+	bltz 	$t1,EXIT 		#neu khong, thoat
+	xor 	$t2,$s2,$s0 	#kiem tra xem $s2 và $s0 co cung dau khong
+	bgez 	$t2,EXIT 		#neu co, thoat
+	li 	$t0,1 		#ke qua la tran so
+EXIT:
