1080facd2196a84b85a86ebb0ddcc53fd757a727
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-04-05 08:25:55 +0700

    Update hello_world.asm

--- MARS tool Introduction/hello_world.asm
+++ MARS tool Introduction/hello_world.asm
@@ -1,11 +1,11 @@
-.data 	#vung du lieu, chua cac khai bao bien
-x: .word	0x01020304	#bien x, khoi tao gia tri
-message: .asciiz	"Hello world!" 
+.data 	#vùng dữ liệu, chứa các khai báo biến
+x: .word	0x01020304	#biến x, khởi tạo giá trị 0x01020304
+message: .asciiz	"Hello world!" #biến message, khởi tạo giá trị "Hello world!"
 
-.text 	#vung lenh, chua cac lenh hop ngu
-la	$a0, message	#dua dia chi bien message vao thanh ghi a0
-li	$v0, 4		#gan thanh ghi v0 = 4
-syscall			#goi ham so v0, ham so 4, la ham print
+.text 	#vùng lệnh, chứa các lệnh hợp ngữ
+la	$a0, message	#đưa địa chỉ của biến message vào thanh ghi $a0
+li	$v0, 4		#gán thanh ghi v0 = 4
+syscall			#gọi hàm số v0, hàm số 4, là hàm print
 
 addi $t1, $zero, 2	#thanh ghi $t1 = 2
 addi $t2, $zero, 3	#thanh ghi $t2 = 3
