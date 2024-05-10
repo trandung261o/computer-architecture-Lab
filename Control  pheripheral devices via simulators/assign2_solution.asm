e74b7db3396c13ab1ae7e1b84b10195a5f1a50e4
Tran Dinh Dung <114976992+trandung261o@users.noreply.github.com>
2024-05-10 07:46:45 +0700

    Update assign2_solution.asm

--- Control  pheripheral devices via simulators/assign2_solution.asm
+++ Control  pheripheral devices via simulators/assign2_solution.asm
@@ -11,11 +11,25 @@
 	#draw a red square
 	li $t0, RED
 	sw $t0, 108($k0)
+	sw $t0, 112($k0)
+	sw $t0, 140($k0)
+	sw $t0, 144($k0)
+	
+	#wait for 3 seconds
+	li $v0, 32
+	la $a0, 3000
+	syscall
 	
 	#moving the square from right to left
-	li $t0, RED
-	sw $t0, 104($k0)
+	#delete old square
+	sw $0, 108($k0)
+	sw $0, 112($k0)
+	sw $0, 140($k0)
+	sw $0, 144($k0)
 	
-	li $t0, 0 
-	sw $t0, 108($k0)
+	#moving square
+	sw $t0, 100($k0)
+	sw $t0, 104($k0)
+	sw $t0, 132($k0)
+	sw $t0, 136($k0)
 
